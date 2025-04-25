f0f5010c6b784142d324ca7744b064322b46db0d
nnasryy <nasrywilliamsbali@gmail.com>
2025-04-25 11:55:10 -0600

    Ejercicio #2

---
+++ Laboratorio_S1/src/laboratorio_s1/Williams_Nasry_Cuotas.java
@@ -0,0 +1,22 @@
+package laboratorio_s1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Williams_Nasry_Cuotas {
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(system.in);
+      
+     System.out.print("Ingrense la cantidad del prestamo: ")
+     double prestamo = scanner.nextDouble();
+  
+     
+    
+    }
+
+    
+    
+    
+}
